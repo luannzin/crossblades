380c8e78ccd18e86683f1e913db5d1787ed4150c
luannzin <luandaniel966@gmail.com>
2024-06-12 22:03:45 -0300

    feat: initial commmit

---
+++ app/(icons)/Play.tsx
@@ -0,0 +1,15 @@
+const PlayIcon = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg
+    viewBox="0 0 24 25"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      d="M20.4086 9.85258C22.5305 11.0065 22.5305 13.9935 20.4086 15.1474L7.59662 22.1145C5.53435 23.236 3 21.7763 3 19.4671L3 5.5329C3 3.22368 5.53435 1.76402 7.59661 2.88548L20.4086 9.85258Z"
+      strokeWidth="1.5"
+    />
+  </svg>
+);
+
+export default PlayIcon;
